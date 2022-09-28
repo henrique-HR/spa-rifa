b8a901c6f3996f1231f1a0a52d70e676c340118c
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2022-09-28 10:26:33 -0300

    quase pronto separei estilos por pagina

--- inicio.js
+++ inicio.js
@@ -1,9 +1,10 @@
 import home from "./pages/home/home.js";
+import sorteio from "./pages/sorteio/sorteio.js";
 
 const main = window.document.querySelector("#root")
 
 window.addEventListener("load", () =>{
 
-        main.appendChild(home());
+        main.appendChild(sorteio());
 
 })
---
+++ pages/sorteio/sorteio.js
@@ -0,0 +1,62 @@
+export default()=>{
+const container = document.createElement('div')
+
+const template = `<div class="imagen">
+            
+</div>
+<div id="vamos">
+    
+    <h1>vamos lá !!!</h1>
+    
+</div>
+<table id="sorteados">
+ <th>nome</th><th>nº</th> 
+ <th>telefone</th>
+ <th>premio</th>
+<tr>
+    <td class="ganhador">boa</td>
+    <td class="no">sorte</td>
+    <td class="tel">a</td>
+    <td class="premio">todos</td>
+</tr>
+<tr>
+    <td class="ganhador">boa</td>
+    <td class="no">sorte</td>
+    <td class="tel">a</td>
+    <td class="premio">todos</td>
+</tr>
+<tr>
+    <td class="ganhador">boa</td>
+    <td class="no">sorte</td>
+    <td class="tel">a</td>
+    <td class="premio">todos</td>
+</tr> 
+<tr>
+    <td class="ganhador">boa</td>
+    <td class="no">sorte</td>
+    <td class="tel">a</td>
+    <td class="premio">todos</td>
+</tr>
+<tr>
+    <td class="ganhador">boa</td>
+    <td class="no">sorte</td>
+    <td class="tel">a</td>
+    <td class="premio">todos</td>
+</tr>
+<tr>
+    <td class="ganhador">boa</td>
+    <td class="no">sorte</td>
+    <td class="tel">a</td>
+    <td class="premio">todos</td>
+</tr>
+</table>
+
+<div id="btnsorte"> 
+    <input type="button" value="embaralhar" onclick="sortear()">
+    <input type="button" value="sortear" onclick="parar()">
+</div>`
+container.innerHTML = template
+
+return container
+
+}
