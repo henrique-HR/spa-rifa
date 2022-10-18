9c694adaa5d885b849c9450cf522ba73c510f534
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2022-10-17 23:11:42 -0300

    atualizaçao do sorteio

--- script.js
+++ script.js
@@ -7,20 +7,94 @@ const escolher=()=>{
   var num = window.document.querySelectorAll('.no')
 
   var con1=[]
-  var nome = ['henrique1 ','roger2   ','gustavo3','priscila4','zoio5','claber','doidin','caue','none','renan','caique','pedro','julia','guilherme']
   
-  var tel =['0' , '1651', '051691' ,'00611','6169161','1913160','16165060','616981651','6169']
-  var premio =  ['combo','pizza','unha','roupa','tenis','coca']
+  const  participante =
+    [
+    {   numero:1,
+        nome:"ana paula ",
+        telefone:'****1295'
+    },{   numero:2,
+        nome:'samara' ,
+        telefone:'****2959'
+    },{
+        numero:5,
+        nome:'mirian' ,
+        telefone: '****6337'
+    },{
+      numero:6,
+      nome:'penha ' ,
+      telefone:'****2776'
+        
+    },{
+        numero:9,
+        nome:'ana lucia ' ,
+        telefone:'****1759'
+    },{
+        numero:10,
+        nome:'juliana ' ,
+        telefone:'****7408'
+    },{
+      numero:11,
+      nome:'juliana ' ,
+      telefone:'****7408'
+    },{
+        numero:13,
+        nome:'eduardo' ,
+        telefone:'****1260'
+    },{
+        numero:17,
+        nome:'betania' ,
+        telefone:'****5055'
+    },{
+        numero:18,
+        nome:'ana deyse' ,
+        telefone:'****9144'
+    },{
+        numero:20,
+        nome:'betania' ,
+        telefone:'****5055'
+    },{
+        numero:23,
+        nome:'vilmara' ,
+        telefone:'****0345'
+    },{
+        numero:25,
+        nome:'juliana' ,
+        telefone:'****7408'
+    },{
+        numero:27,
+        nome:'elisangela' ,
+        telefone:'****6057'
+    },{
+      numero:36,
+      nome:'caua',
+      telefone:'****4582'
+    },{
+        numero:56,
+        nome:'elita' ,
+        telefone:'****7299'
+    },{
+        numero:66,
+        nome:'cleide' ,
+        telefone:'***1102'
+    },{
+        numero:100,
+        nome:'ana carla',
+        telefone:'****6218'
+    }
+]
+
+  var premio =  ['combo 2 pizzas + refri','aplicaçao de unha em gel ','combo galeto + feijoada ','conjunto de roupa fit (F)','combo x-tudo + batata + refri(250 ml','pix R$ 50,00']
   var i=0,j=0,igual,vetor=[]
 
   
   do{
-        vetor[i]= Math.floor(Math.random()*14)
+        vetor[i]= Math.floor(Math.random()*18)
 
         igual=0
 
         for( j=0; j<i ;j++){
-            if(vetor[j]==vetor[i] ){
+            if(vetor[j]==vetor[i]){
               igual=1
             }
         }if(igual==0){
@@ -30,27 +104,24 @@ const escolher=()=>{
         
   }while( i <6 )
 
-  for(i=0;i<6;i++){
+ for(i=0;i<6;i++){
     
     con1[i]=vetor[i]
     
-    ganhador[i].innerHTML =`<strong>${i+1}º</strong> ${nome[con1[i]]}`
-    teu[i].innerHTML = ` ${tel[con1[i]]} `
-    num[i].innerHTML = `${vetor[i]+1}`
+    ganhador[i].innerHTML =`<strong>${i+1}º</strong> ${participante[con1[i]].nome}`
+    teu[i].innerHTML = ` ${participante[con1[i]].telefone} `
+    num[i].innerHTML = `${participante[con1[i]].numero}`
     pre[i].innerHTML = `${premio[i]}`
   }
-
   
+ 
   
 }
 const sortear =()=>{
 
- set = setInterval(escolher,100)
+ set = setInterval(escolher,200)
 
 }
-
-
-
 const parar = ()=>{
   clearInterval(set)
 }
